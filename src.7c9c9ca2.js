b862b8d9ef3b78c9a167f3bdab578fa4ee5264b3
MateuszTyb <86377061+MateuszTyb@users.noreply.github.com>
2021-09-09 08:05:30 +0000

    Deploying to gh-pages from @ MateuszTyb/GoITGroup3Project1@61bf230d73a3327b1f0d2b39815adb75fabc1c36 🚀

--- src.7c9c9ca2.js
+++ src.7c9c9ca2.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/GoITGroup3Project1/src.43ee2809.js.map
+//# sourceMappingURL=/GoITGroup3Project1/src.7c9c9ca2.js.map
